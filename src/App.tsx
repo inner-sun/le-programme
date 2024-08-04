837df3eb168910c636fdaa137f82ad1eb254dd87
InnerSun <167511709+inner-sun@users.noreply.github.com>
2024-08-04 18:35:04 +0200

    Basic SolidJS barebones project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js'
+import '~/styles/reset.scss'
+import '~/styles/fonts.scss'
+import '~/styles/app.scss'
+
+const App: Component = () => {
+  return (
+    <div>
+      yay
+    </div>
+  )
+}
+
+export default App
